54c135698716741e13bad523156b94e29b3b2b4f
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-08-25 11:53:12 +0300

    Create Person custom collectionView cell with its class

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by othman shahrouri on 8/25/21.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
--- Project10/ViewController.swift
+++ Project10/ViewController.swift
@@ -7,12 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
     }
+    
 
 
 }
