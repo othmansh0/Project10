041780de6ac330c31a000d6480babeeee4f8d02f
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-08-25 14:06:15 +0300

    Implement numberOfItemsInSection and cellForItemAt

--- Project10/ViewController.swift
+++ Project10/ViewController.swift
@@ -13,7 +13,22 @@ class ViewController: UICollectionViewController {
         super.viewDidLoad()
     
     }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
     
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        //This function returns a cell of type UICollectionViewCell rather than a Person cell
+        //We need to type cast it to our custom cell
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            // we failed to get a PersonCell – bail out!
+            fatalError("Unable to deuque PersonCell")
+        }
+       
+        
+        
+        return cell
+    }
 
 
 }
