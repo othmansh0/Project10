cb9dda017e624c48533eef9231e1829a206962ef
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-08-25 21:22:41 +0300

    Implement didSeltItemAt and Person class

---
+++ Project10/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by othman shahrouri on 8/25/21.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+
+}
--- Project10/ViewController.swift
+++ Project10/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 //UINavigationControllerDelegate:
 
 class ViewController: UICollectionViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    var people = [Person]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +19,7 @@ class ViewController: UICollectionViewController,UIImagePickerControllerDelegate
     
     }
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return people.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -29,9 +30,39 @@ class ViewController: UICollectionViewController,UIImagePickerControllerDelegate
             fatalError("Unable to deuque PersonCell")
         }
        
+        //1.Pull out the person from the people array at the correct position
+        let person = people[indexPath.item]
+        //2.Set the name label to the person's name
+        cell.name.text = person.name
+        //3.Create a UIImage from the person's image filename
+        //adding it to the value from getDocumentsDirectory() so that we have a full path for the image
+        let path = getDocumentsDirectory().appendingPathComponent(person.image)
+        cell.imageView.image = UIImage(contentsOfFile: path.path)
         
+        cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
         
         return cell
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let ac = UIAlertController(title: "Add name", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        let action = UIAlertAction(title: "Add", style: .default) {[weak self,weak ac]
+            action in
+            guard let newName = ac?.textFields?[0].text else {return}
+            self?.people[indexPath.item].name = newName
+            collectionView.reloadData()
+            
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        ac.addAction(action)
+        ac.addAction(cancelAction)
+        present(ac, animated: true)
+      
     }
 
 
@@ -76,6 +107,10 @@ class ViewController: UICollectionViewController,UIImagePickerControllerDelegate
         if let jpegData = image.jpegData(compressionQuality: 0.8){
             try? jpegData.write(to: imagePath)
         }
+        
+        let person = Person(name: "Othman", image: imageName)
+        people.append(person)
+        collectionView.reloadData()
         
         dismiss(animated: true)
         
